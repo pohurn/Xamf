2a3cee79e7031e8ccc19573c1fad1b838ec19db9
Navindranath Pohur <pohur95@gmail.com>
2021-03-08 11:25:44 +0400

    intial commit

---
+++ xamf/xamf/Exceptions/ApiException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace xamf.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+        public string Name { get; }
+        public string Content { get; }
+
+        public ApiException()
+        {
+        }
+
+        public ApiException(HttpStatusCode statusCode, string name, string content)
+        {
+            StatusCode = statusCode;
+            Name = name;
+            Content = content;
+        }
+    }
+}
