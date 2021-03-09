9847e547311ef8b289caad5ccdf164eef7c56f30
Navindranath Pohur <pohur95@gmail.com>
2021-03-09 11:40:33 +0400

    added model

---
+++ xamf/xamf/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamf.Models
+{
+    public class Recipe
+    {
+        public string title { get; set; }
+        public double version { get; set; }
+        public string href { get; set; }
+        public List<menus> results { get; set; }
+    }
+
+    public class menus
+    {
+        public string title { get; set; }
+        public string href { get; set; }
+        public string ingredients { get; set; }
+        public string thumbnail { get; set; }
+    }
+}
