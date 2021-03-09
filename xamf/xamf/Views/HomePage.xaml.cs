9418776e7f27e5b506a8d9742927622ce9662e70
Navindranath Pohur <pohur95@gmail.com>
2021-03-09 14:18:40 +0400

    share recipe now available

--- xamf/xamf/Views/HomePage.xaml.cs
+++ xamf/xamf/Views/HomePage.xaml.cs
@@ -2,12 +2,14 @@
 using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using xamf.Models;
 using xamf.Services;
 using xamf.Services.RequestProvider;
 using xamf.ViewModels;
@@ -17,6 +19,9 @@ namespace xamf.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        bool canOpenRecipe = false;
+        bool canShareRecipe = false;
+
         public HomePage()
         {
             InitializeComponent();
@@ -30,9 +35,55 @@ namespace xamf.Views
             ((RecipeViewModel)BindingContext).FetchRecipesCommand.Execute(null);
         }
 
-        private void OnSelectingRecipe(object sender, SelectionChangedEventArgs e)
+        private async void OnSelectingRecipe(object sender, SelectionChangedEventArgs e)
         {
+            if (canOpenRecipe) return;
+            canOpenRecipe = true;
+            var item = e.CurrentSelection;
+            if (item.Count == 1)
+            {
+                var data = item.FirstOrDefault();
+                if (data != null)
+                {
+                    Debug.WriteLine("yes");
+                    var datamodel = (menus)data;
 
+                    openUrl(datamodel.href);
+
+                }
+            }
+            canOpenRecipe = false;
+            ((CollectionView)sender).SelectedItem = null;
+        }
+
+        public async void openUrl(string url)
+        {
+            await Browser.OpenAsync(url, new BrowserLaunchOptions
+            {
+                LaunchMode = BrowserLaunchMode.SystemPreferred,
+                TitleMode = BrowserTitleMode.Show,
+                PreferredToolbarColor = Color.AliceBlue,
+                PreferredControlColor = Color.Violet
+            });
+        }
+
+        private void OnShareIconTapped(object sender, EventArgs e)
+        {
+            var tappedItem = (TappedEventArgs)e;
+            var ItemSelected = (menus)tappedItem.Parameter;
+            if (canShareRecipe) return;
+            canShareRecipe = true;
+            ShareUri(ItemSelected.href, ItemSelected.title);
+            canShareRecipe = false;
+        }
+
+        public async Task ShareUri(string uri, string recipeTitle)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Uri = uri,
+                Title = recipeTitle
+            });
         }
     }
 }
